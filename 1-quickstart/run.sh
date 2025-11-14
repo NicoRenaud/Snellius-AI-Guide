f7ed6fbff6d64277bef9805ba95f560b88495f67
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2025-11-14 08:58:09 +0100

    fix arg

--- 1-quickstart/run.sh
+++ 1-quickstart/run.sh
@@ -7,7 +7,7 @@
 
 # enter your project space path e.g. PROJECT_SPACE=/projects/0/prjsXXXX
 # if you didn't add the path to your bashrc
-PROJECT_SPACE=/scracth-shared/nicolasr
+PROJECT_SPACE=/scratch-shared/nicolasr
 
 if [ -z "$PROJECT_SPACE" ]; then
   echo "Error: PROJECT_SPACE is not set. Please set the project space path in this script. Example: export PROJECT_SPACE=/projects/0/prjsXXXX"
@@ -16,8 +16,8 @@ fi
 
 
 CONTAINER=$PROJECT_SPACE/containers/snellius-ai-guide-torch-2.7-nvcr.25-10.sif
-IMAGENET=$PROJECT_SPACE/datasets/imagenet/imagenet-200.hf
+IMAGENET=$PROJECT_SPACE/datasets/imagenet/tiny-imagenet-200.hf
 WORKERS=${SLURM_CPUS_PER_TASK:-16}
 BIND_PATH=$PROJECT_SPACE
 
-apptainer exec -B $BIND_PATH $CONTAINER python visiontransformer.py --data_path $IMAGENET --workers $WORKERS
+apptainer exec -B $BIND_PATH $CONTAINER python visiontransformer.py --data_path $IMAGENET --num_workers $WORKERS
